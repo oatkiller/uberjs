a3278ff22451e7f4b6198b35c26c17a48e2e3af9
Robert Austin <robert@oatlab.com>
2011-04-17 20:32:22 -0400

    derps

--- tests.js
+++ tests.js
@@ -11,197 +11,167 @@ var suite = new Suite({
 	tearDown : function () {
 	},
 
-	'getUber gets the right superclass method when inheritance is setup with vanilla js' : function () {
-		// make a uber class
-		var UberClass = function () {};
-
-		// give it a method
-		UberClass.prototype.derp = function () {
-			return 'a';
+	// extend
+	'extend creates a sub class' : function () {
+		var Uber = function () {
 		};
+		var Sub = Uber.extend();
+		Assert((new Sub()) instanceof Uber,'extend didnt work');
+	},
 
-		// give it a reference to getUber. this is usually done by Function.prototype.subclass, but I'm not testing that in this test
-		UberClass.prototype.getUber = Function.getUber;
-
-		// make a sub class
-		var SubClass = function () {};
-
-		// setup inheritance, vanilla js style
-
-		// make a constructor that will create a prototype that inherits from UberClass.prototype
-		var C = function () {};
-
-		// make the new constructor inherit from UberClass's prototype
-		C.prototype = UberClass.prototype;
-
-		// create Sub's prototype, which inherits from UberClass's prototype
-		SubClass.prototype = new C();
-
-		// set Sub's prototype's constructor to SubClass, otherwise it will inherit UberClass.prototype's constructor property which points to UberClass
-		SubClass.prototype.constructor = SubClass;
-
-		// give SubClass a derp method
-		SubClass.prototype.derp = function () {
-			// assert that getUber gets the right method
-			Assert(this.getUber(arguments) === UberClass.prototype.derp,'getUber didnt work');
+	// extend extend
+	'extend works on a subclass' : function () {
+		var Uber = function () {
 		};
+		var Sub = Uber.extend();
+		var SubSub = Sub.extend();
+		var instance = new SubSub();
+		Assert(instance instanceof Sub && instance instanceof Uber,'extend didnt work');
+	},
 
-		// create a subclass instance
-		var subClass = new SubClass();
+	// extends
+	'extends forces an existing class to inherit from an uber class. existing methods are preserved' : function () {
+		var Sub = function () {
+		};
+		Sub.prototype.derp = function () {
+		};
+		var Uber = function () {
+		};
+		Sub.extends(Uber);
+		var instance = new Sub();
+		Assert(instance instanceof Uber,'extends didnt work');
+		Assert('derp' in instance,'extends didnt preserve existing prototype');
+	},
 
-		// call derp on the subclass
-		subClass.derp();
+	// uber on first uber
+	'uber calls the uber version of a method, passing all the same arguments' : function () {
+		var Uber = function () {
+			this.a = 'a';
+		};
+		Uber.prototype.derp = function () {
+			return this.a;
+		};
+		var Sub = Uber.extend({
+			b : 'b',
+			derp : function () {
+				return this.b + this.uber(arguments);;
+			}
+		});
+		var instance = new Sub();
+		Assert(instance.derp() === 'ba','uber didnt work well');
+	},
+
+	// uber that calls uber
+	'uber can work two levels deep' : function () {
+		var Uber = function () {
+			this.a = 'a';
+		};
+		Uber.prototype.derp = function () {
+			return this.a;
+		};
+		var Sub = function () {
+			this.uber(arguments);
+		};
+		Sub.prototype = {
+			constructor : Sub,
+			b : 'b',
+			derp : function () {
+				return this.b + this.uber(arguments);
+			}
+		};
+		Sub.extends(Uber);
+		var SubSub = Sub.extend({
+			c : 'c',
+			derp : function () {
+				return this.c + this.uber(arguments);
+			}
+		});
+		var instance = new SubSub();
+		Assert(instance.derp() === 'cba','didnt work');
 
 	},
 
-	'getUber works when there is a subclass thats also a super and doesnt have the submethod' : function () {
-		// make a super class
-		var UberClass = function () {
+	// uber that skips a uber class
+	'uber can find a uber method thats not it its prototypes prototype' : function () {
+		var Uber = function () {
+			this.a = 'a';
 		};
-		// give it a uber method
-		UberClass.prototype.derp = function () {
-			return 'a';
+		Uber.prototype.derp = function () {
+			return this.a;
 		};
-
-		// put getUber in the prototype. this is usually handled by .subclass, but we aren't testing that here
-		UberClass.prototype.getUber = Function.getUber;
-
-		// make a subclass
-		var SubClass = function () {
-		};
-
-		// make a prototype constructor
-		var C = function () {};
-
-		// it will inherit from UberClass
-		C.prototype = UberClass.prototype;
-
-		// create the new prototype
-		SubClass.prototype = new C();
-
-		// overwrite the constructor property
-		SubClass.prototype.constructor = SubClass;
-
-		// make a sub sub class
-	 	var SubSubClass = function () {};
-
-		// make a function to create its constructor
-		var CC = function () {};
-
-		// it will inherit from subclass
-		CC.prototype = SubClass.prototype;
-
-		// create the new constructor
-		SubSubClass.prototype = new CC();
-
-		// update its constructor property
-		SubSubClass.prototype.constructor = SubSubClass;
-
-		// give it a derp method
-		SubSubClass.prototype.derp = function () {
-			// assert that getUber gets the top level class's derp method
-			Assert(this.getUber(arguments) === UberClass.prototype.derp,'getUber didnt work');
-		};
-
-		// create the sub sub class
-		var subSubClass = new SubSubClass();
-
-		// call its derp
-		subSubClass.derp();
-
-	},
-
-	'Function.prototype.subclass takes an initializer and uses the constructor to make a subclass of this' : function () {
-
-		// create an uber class
-		var UberClass = function () {
-		};
-
-		// set a property: b on its prototype
-		UberClass.prototype.b = 2;
-
-		// set another property
-		UberClass.prototype.c = 2;
-
-		// create a subclass using the subclass method
-		var SubClass = UberClass.subclass({
-			// the subclass will have a property b set to 1
-			b : 1
+		var Sub = Uber.extend();
+		var SubSub = Sub.extend({
+			b : 'b',
+			derp : function () {
+				return this.b + this.uber(arguments);
+			}
 		});
 
-		// create a new subclass
-		var subClass = new SubClass();
-
-		// assert that is returns instanceof properly
-		Assert(subClass instanceof SubClass,'subClass isnt an instance of SubClass');
-
-		// assert that is returns instanceof properly for its uber
-		Assert(subClass instanceof UberClass,'subClass isnt an instance of UberClass');
-
-		// assert that the subclass overwrites the uber
-		Assert(subClass.b === 1,'property wasnt overridden');
-
-		// assert that the subclass can access properties on the UberClass's prototype
-		Assert(subClass.c === 2,'property not accessible');
+		var instance = new SubSub();
+		Assert(instance.derp() === 'ba','nopes');
 	},
 
-	'Function.prototype.subclass will assign a default constructor if none is provided. this constructor calls its uber' : function () {
-		// create an uber with a constructor that sets an instance property
-		var UberClass = function () {
-			this.a = 1;
+	// uber can call a constructor
+	'uber works with constructors' : function () {
+		var Uber = function () {
+			this.a = 'a';
 		};
-
-		// create an identity subclass
-		var SubClass = UberClass.subclass({});
-
-		// create an instnace of the subclass
-		var subClass = new SubClass();
-
-		// assert that its constructor was called, and that it had access to 'this'
-		Assert(subClass.a === 1,'uber constructor wasnt called');
+		var Sub = function () {
+			this.uber(arguments);
+		};
+		Sub.extends(Uber);
+		var instance = new Sub();
+		Assert(instance.a === 'a','no');
 	},
 
-	'Function.prototype.subclasses will force an existing class to inherit from the passed class.' : function () {
-		var SubClass = function (payload) {
-			this.a = this.getUber(arguments).apply(this,arguments) + ',suba: ' + payload;
+	// getUber works
+	'getUber works too' : function () {
+		var uberDerp = function () {
 		};
-		SubClass.prototype = {
-			b : 2
-		};
-		var UberClass = function (payload) {
-			this.a = 'ubera: ' + payload;
-		};
-		UberClass.prototype = {
-			b : 1
-		};
-		
-		SubClass.subclasses(UberClass);
 
-		var SubSubClass = SubClass.subclass({
-			constructor : function (payload) {
-				this.a = this.getUber(arguments).apply(this,arguments) + ',subsuba: ' + payload;
+		var Uber = function () {
+		};
+		Uber.prototype.derp = uberDerp;
+		var Sub = function () {
+		};
+		Sub.extends(Uber);
+		Sub.prototype.derp = function () {
+			return this.getUber(arguments);;
+		};
+		var instance = new Sub();
+		Assert(instance.derp() === uberDerp,'no');
+	},
+
+	// getUber can get a constructor
+	'getUber can resolve a constructor' : function () {
+		var subConstructorRan = false;
+		var Uber = function () {
+		};
+		var Sub = Uber.extend({
+			constructor : function () {
+				subConstructorRan = true;
+				Assert(this.getUber(arguments) === Uber,'no');
+			}
+		});
+		var instance = new Sub();
+		Assert(subConstructorRan === true,'noo');
+	},
+
+	// klass works
+	'klass creates a constructor' : function () {
+		var Uber = Function.klass({
+			constructor : function () {
+				this.a = 'a';
 			},
-			b : 3
+			derp : function () {
+				return this.a + 'b';
+			}
 		});
-
-		var payload = '!';
-
-		var uberClass = new UberClass(payload);
-		Assert(uberClass.b === 1,'property didnt set right to begin with');
-		Assert(uberClass.a === 'ubera: !');
-
-		var subClass = new SubClass(payload);
-		Assert(subClass.b === 2,'property from prototype didnt overwrite');
-		Assert(subClass.a === 'ubera: !,suba: !','constructor didnt call uber constructor properly');
-
-		var subSubClass = new SubSubClass(payload);
-		Assert(subSubClass.b === 3,'property from prototype didnt overwrite');
-		Assert(subSubClass.a === 'ubera: !,suba: !,subsuba: !','constructor didnt call uber constructor properly');
-	},
-
-	'Function.prototype.subclasses will throw an error if you try to break an existing inheritance' : function () {
-		Assert(false,'not implemented');
+		var instance = new Uber();
+		Assert(instance instanceof Uber,'no');
+		Assert(instance.derp() === 'ab','no');
 	}
+
 
 });
 
--- uber.js
+++ uber.js
@@ -1,119 +1,15 @@
 (function () {
-	var getMethodName = function (callee,prototype) {
-						
-		// find the name of the method we are calling
-		for (var propertyName in prototype) {
-			if (prototype[propertyName] === callee) {
-				return propertyName;
-			}
-		}
-	};
-
-	Function.getUber = function (args) {
-
-		var methodName = getMethodName(args.callee,this.constructor.prototype);
-
-		// if we dont find it, throw an error
-		if (typeof methodName === 'undefined') {
-			throw new Error('couldn\'t find this method');
-		}
-
-		// look on this prototype
-		return (function (constructor) {
-			var prototype = constructor.prototype;
-
-			// if you find it
-			if (prototype.hasOwnProperty(methodName)) {
-				// get the next thing beneath it
-
-				// temporarily remove the method
-				delete prototype[methodName];
-
-				// look underneath
-				var superMethod = prototype[methodName];
-
-				// put the method back
-				prototype[methodName] = args.callee;
-
-				// is it defined?
-				if (typeof superMethod === 'undefined') {
-					// if not, throw an error
-					throw new Error('couldn\'t find a uber method by the name of: ' + methodName);
-				} else {
-					// if so, return it
-					return superMethod;
-				}
-			} else {
-				// if not, is there another prototype?
-				delete constructor.prototype.constructor;
-				var nextConstructor = constructor.prototype.constructor;
-				constructor.prototype.constructor = constructor;
-
-				if (typeof nextConstructor === 'undefined') {
-					// if constructor isnt defined, throw an error
-					throw new Error('couldn\'t navigate prototype using constructor property.');
-				} else if (constructor === Object) {
-					// if constructor is Object, throw an error
-					throw new Error('walked all the way back to Object without finding your method');
-				} else {
-					// if so, look there
-					return arguments.callee(nextConstructor);
-				}
-			}
-		})(this.constructor);
-	};
-})();
-
-Function.prototype.subclass = function (initializer) {
-	if (!('getUber' in this.prototype)) {
-		this.prototype.getUber = Function.getUber;
-	}
-
-	// get the passed constructor, or create a default one that just calls its uber
-	var constructor = 'constructor' in initializer && initializer.constructor !== Object ? initializer.constructor : function () {
-		this.getUber(arguments).apply(this,arguments);
-	};
-
-	// create a throw away function that will construct the new prototype
-	var PrototypeConstructor = function () {};
-
-	// set this prototype to the PrototypeConstructor's prototype so that the new prototype will inherit from this
-	PrototypeConstructor.prototype = this.prototype;
-
-	// create the new prototype
-	var prototype = new PrototypeConstructor();
-
-	// set the new prototype on the new constructor
-	constructor.prototype = prototype;
-
-	// set the new prototype's constructor to the new constructor. otherwise, it will point to this
-	prototype.constructor = constructor;
-
-	// copy properties to new constructor
-	for (var propertyName in initializer) {
-		// dont worry about ignoring constructor, if thats there. just rewrite it. doing that should be faster than doing a bunch of false ifs
-		prototype[propertyName] = initializer[propertyName];
-	}
-
-	// return the new fancy constructor
-	return constructor;
-};
-
-/*
-(function () {
 	// get a super method by its sub method
 	// argsOrMethod is a reference to arguments or arguments.callee
 	// requires prototype's to have a reference to their constructor and a reference to themselves as .prototype
-	// use Function.prototype.subclass and Function.prototype.subclasses
+	// use Function.prototype.extend and Function.prototype.extends
 	var getUber = function (argsOrMethod) {
 
 		// get the sub method that we are interested in. either it was passed, or arguments was passed in which case use arguments.callee
 		var method = typeof argsOrMethod === 'function' ? argsOrMethod : argsOrMethod.callee;
 
 		// recurse the prototypal chain looking for the method that was called. return the next property in the chain by the same property name
-		return (function (constructor) {
-
-			var prototype = constructor.prototype;
+		return (function (prototype) {
 
 			// for each property in the prototype
 			for (var methodName in prototype) {
@@ -122,16 +18,14 @@ Function.prototype.subclass = function (initializer) {
 				if (prototype[methodName] === method && prototype.hasOwnProperty(methodName)) {
 
 					// delete the circular reference from the prototype
-					delete prototype.constructor;
+					delete prototype.prototype;
 
 					// get a reference to the next property in the prototypal chain by the same property name
 					// prototype.prototype points to the next prototype in the chain because we just deleted the current prototype's circular reference, thereby revealing its internal prototype's circular reference
-					var superMethod = prototype.constructor.prototype[methodName];
+					var superMethod = prototype.prototype[methodName];
 
 					// restore the circular reference
-					prototype.constructor = constructor;
-
-					// TODO throw an error is superMethod is undefined
+					prototype.prototype = prototype;
 
 					// return whatever we found
 					return superMethod;
@@ -141,18 +35,18 @@ Function.prototype.subclass = function (initializer) {
 			// we didnt find what we were looking for
 
 			// delete this prototype's circular reference
-			delete prototype.constructor;
+			delete prototype.prototype;
 
 			// get its parent prototype by its parents prototypes now revealed circular reference
-			var nextConstructor = prototype.constructor;
+			var nextPrototype = prototype.prototype;
 
 			// restore the circular reference
-			prototype.constructor = constructor;
+			prototype.prototype = prototype;
 
 			// if there was another prototype in our circular reference chain, call this method on that one, otherwise, return undefined
-			return typeof nextConstructor === 'function' && nextConstructor !== Object ? arguments.callee(nextConstructor) : undefined;
+			return nextPrototype ? arguments.callee(nextPrototype) : undefined;
 
-		})(this.constructor); // call the anon fn with this.constructor
+		})(this.prototype); // call the anon fn with this.prototype
 	},
 
 	// pass arguments and optionally an array of args
@@ -162,7 +56,7 @@ Function.prototype.subclass = function (initializer) {
 		return this.getUber(args).apply(this,optionalArgs || args);
 	},
 
-	// used by .subclass and .subclasses
+	// used by .extend and .extends
 	setupInheritance = function (superclass,constructor,oldPrototype) {
 
 		// create an empty constructor, used to get an object that inherits from the superclass proto
@@ -174,7 +68,7 @@ Function.prototype.subclass = function (initializer) {
 		var prototype = new C();
 
 		// for any properties on oldPrototype, copy them to new prototype
-		// this will occur if the implementor added properties to the prototype before calling an subclass method
+		// this will occur if the implementor added properties to the prototype before calling an extend method
 		for (var property in oldPrototype) {
 			// only copy properties that are dircetly on the oldPrototype.
 			// any old inheritance will be lost.
@@ -186,7 +80,12 @@ Function.prototype.subclass = function (initializer) {
 		// set the prototype on the constructor
 		constructor.prototype = prototype;
 
+		// give the prototype a circular reference
+		// this is used to transverse the prototypal chain
+		prototype.prototype = prototype;
+
 		// give the prototype an explicit reference to its constructor
+		// this is used to identify constructors for the sake of allowing uber constructors
 		prototype.constructor = constructor;
 
 		// if uber isnt in the prototype chain, add it now
@@ -203,18 +102,21 @@ Function.prototype.subclass = function (initializer) {
 
 			// set the superclass's prototype's constructor so that we can call it with this.uber from the constructor function
 			superclass.prototype.constructor = superclass;
+
+			// set the superclass's prototype's circular reference so that we can look for uber methods on it in our prototype transversing routine
+			superclass.prototype.prototype = superclass.prototype;
 		}
 
 		// return the constructor
 		return constructor;
 	};
 
-	// SubClass.subclasses(SuperClass); // setup a dynamic inheritance
-	Function.prototype.subclasses = function (superclass) {
+	// SubClass.extends(SuperClass); // setup a dynamic inheritance
+	Function.prototype.extends = function (superclass) {
 		return setupInheritance(superclass,this,this.prototype);
 	};
 
-	// 	var SubClass = SuperClass.subclass({ // returns a constructor with a prototype formed from the object you pass. if your object has a constructor property, that will be returned.
+	// 	var SubClass = SuperClass.extend({ // returns a constructor with a prototype formed from the object you pass. if your object has a constructor property, that will be returned.
 	//  	// optional
 	// 		constructor : function () {
 	//			...
@@ -223,11 +125,9 @@ Function.prototype.subclass = function (initializer) {
 	// 		},
 	// 		...
 	//	});
-	Function.prototype.subclass = function (properties) {
-		properties = typeof properties === 'undefined' ? {} : properties;
-
+	Function.prototype.extend = function (properties) {
 		// if the properties object didnt define a constructor, use a generic one that just calls its uber
-		var constructor = properties.hasOwnProperty('constructor') ? properties.constructor : function () {
+		var constructor = properties && properties.hasOwnProperty('constructor') ? properties.constructor : function () {
 			this.uber(arguments);
 		};
 
@@ -235,17 +135,11 @@ Function.prototype.subclass = function (initializer) {
 		return setupInheritance(this,constructor,properties);
 	};
 
-	// just incase you want to create a class without a special constructor. if you dont want this pollution, delete it.
-	Function.klass = function (prototype) {
-		// if a constructor wasnt passed, add one
-		prototype.constructor = prototype.hasOwnProperty('constructor') ? prototype.constructor : function () {};
-
-		// set the constructors prototype
-		prototype.constructor.prototype = prototype;
-
-		// return the constructor
-		return prototype.constructor;
+	Function.klass = function (properties) {
+		var constructor = properties.hasOwnProperty('constructor') ? properties.constructor : function () {};
+		properties.constructor = constructor;
+		constructor.prototype = properties;
+		return constructor;
 	};
 
 })();
-*/
